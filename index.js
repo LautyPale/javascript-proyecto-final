3183439e1687ab4ac0a2f8010d340056ccab4ebe
pale <palenalautaro@gmail.com>
2024-02-27 17:08:05 -0300

    Mejora del codigo, cambio variables por objetos. agrego funciones de orden superior, arrays y manejo de arrays

--- index.js
+++ index.js
@@ -1,69 +1,104 @@
-function obtenerTipoDeCambio(monedaOrigen, monedaDestino) { // pido la monedaOrigen y la monedaDestino
-    const tipoDeCambio = { // Realizo un objeto con los tipos de cambio y su propio valor (1) para evitar errores al pedirle la monedaOrigen o monedaDestino
-        "EUR": { 
-            "USD": 1.08,
-            "BRL": 5.41,
-            "ARS": 907.56,
-            "EUR": 1
-        },
-        "USD": {
-            "EUR": 0.92,
-            "BRL": 5.00,
-            "ARS": 837.50,
-            "USD": 1
-        },
-        "BRL": {
-            "EUR": 0.18,
-            "USD": 0.20,
-            "ARS": 167.98,
-            "BRL": 1
-        },
-        "ARS": {
-            "EUR": 0.0011,
-            "USD": 0.0012,
-            "BRL": 0.0060,
-            "ARS": 1
-        },
-    }
+// Defino constantes
 
-    //return tipoDeCambio [monedaOrigen][monedaDestino] // retorno el valor de la moneda que elija el usuario
+const VALOR_USD = 1
+const VALOR_EUR = 1.08 * VALOR_USD
+const VALOR_BRL = 0.2 * VALOR_USD
+const VALOR_ARS = 0.0009 * VALOR_USD
+const historial = [];
+
+// Objeto con los tipos de cambio
+
+tipoDeCambio = {
+    "USD": VALOR_USD,
+    "EUR": VALOR_EUR,
+    "BRL": VALOR_BRL,
+    "ARS": VALOR_ARS
 }
 
-function convertirMoneda(cantMoneda, monedaOrigen, monedaDestino) {
-    const tipoDeCambio = obtenerTipoDeCambio(monedaOrigen, monedaDestino)
-    return cantMoneda * tipoDeCambio
+// Funcion para convertir divisas
+
+function convertirMoneda(monedaElegida, monedaAConvertir, cantidadMonedaElegida, guardarHistorial) {
+  
+    const tipoCambioOrigen = tipoDeCambio[monedaElegida];
+    const tipoCambioDestino = tipoDeCambio[monedaAConvertir];
+  
+    const cantidadConvertida = (cantidadMonedaElegida * tipoCambioOrigen) / tipoCambioDestino;
+
+    guardarHistorial(monedaElegida, monedaAConvertir, cantidadMonedaElegida, cantidadConvertida.toFixed(2), );
+
+    return cantidadConvertida.toFixed(2);
 }
 
-function mostrarResultado(cantMoneda, monedaOrigen, monedaDestino, resultado) { 
-    // cantMoneda = cantidad a convertir
-    // monedaOrigen = moneda que elija el usuario
-    // monedaDestino = moneda que elija el usuario
-    // resultado = utilizo la funcion convertirMoneda
-    alert(`La cantidad de ${cantMoneda} ${monedaOrigen} equivalen a ${resultado.toFixed(2)} ${monedaDestino}`) // .toFixed es para redondear el resultado
+// Funcion para guardar historial
+
+function guardarHistorial(monedaElegida, monedaAConvertir, cantidad, resultado) {
+
+    const historialConversion = {
+        monedaElegida: monedaElegida,
+        monedaAConvertir: monedaAConvertir,
+        cantidadMonedaElegida: cantidad,
+        resultado: resultado
+    }
+
+    historial.push(historialConversion);
 }
 
-// Inicializo las variables
+// Defino variables
 
-let monedaOrigen;
-let monedaDestino;
-let cantidad;
+let menu
+let monedaElegida
+let monedaAConvertir
+let cantidad
+let resultado
 
-do { // pido la moneda que quiere convertir el usuario
-    monedaOrigen = prompt("Ingrese la moneda de origen (EUR, USD, BRL, ARS):").toUpperCase();
-    if (!["EUR", "USD", "BRL", "ARS"].includes(monedaOrigen)) { // .includes es para saber si monedaOrigen tiene el elemento (EUR, USD, BRL, ARS) y confirmar que no sea falso
-        alert("La moneda que desea convertir no es válida. Por favor, ingrese una moneda válida (EUR, USD, BRL, ARS).")
+// Menu
+
+while (menu !== '0') {
+
+    let mensaje = `Ultimas conversiones: \n`
+
+    menu = prompt("Bienvenido al conversor de divisas. ¿Que deseas hacer? \n" +
+        "1. Convertir divisas \n" +
+        "2. Ver historial \n" +
+        "0. Salir")
+    
+    if (menu === '1') {
+
+        do {
+            monedaElegida = prompt("Ingrese la moneda que desea convertir (EUR, USD, BRL, ARS):").toUpperCase();
+            if (!["EUR", "USD", "BRL", "ARS"].includes(monedaElegida)) { // .includes es para saber si moneda tiene el elemento (EUR, USD, BRL, ARS) y confirmar que no sea falso
+                alert("La moneda que desea convertir no es válida. Por favor, ingrese una moneda válida (EUR, USD, BRL, ARS).")
+            }
+        } while (!["EUR", "USD", "BRL", "ARS"].includes(monedaElegida));
+
+        do {
+            monedaAConvertir = prompt("Ingrese la moneda a la que desea convertir (EUR, USD, BRL, ARS):").toUpperCase();
+            if (!["EUR", "USD", "BRL", "ARS"].includes(monedaAConvertir)) {
+                alert("La moneda que desea convertir no es válida. Por favor, ingrese una moneda válida (EUR, USD, BRL, ARS).")
+            }
+        } while (!["EUR", "USD", "BRL", "ARS"].includes(monedaAConvertir));
+
+        cantidad = parseFloat(prompt("Ingresa la cantidad que deseas convertir"));
+
+        resultado = convertirMoneda(monedaElegida, monedaAConvertir, cantidad, guardarHistorial);
+        alert(`El resultado de la conversión es: ${resultado}`);
+
     }
-} while (!["EUR", "USD", "BRL", "ARS"].includes(monedaOrigen));
 
-do { // pido la moneda a la que quiere convertir el usuario
-    monedaDestino = prompt("Ingrese la moneda de destino (EUR, USD, BRL, ARS):").toUpperCase();
-    if (!["EUR", "USD", "BRL", "ARS"].includes(monedaDestino)) {
-        alert("La moneda que desea convertir no es válida. Por favor, ingrese una moneda válida (EUR, USD, BRL, ARS).")
+    if (menu === '2') {
+        historial.forEach(element => {
+            mensaje = mensaje + `$${element.cantidadMonedaElegida} ${element.monedaElegida} = $${element.resultado} ${element.monedaAConvertir} \n`
+        })
+
+        alert(mensaje)
     }
-} while (!["EUR", "USD", "BRL", "ARS"].includes(monedaDestino));
 
-cantidad = parseFloat(prompt("Ingresa la cantidad que deseas convertir")) // parseFloat para convertir a entero
+    if (menu === '0') {
+        alert("Hasta pronto!")
+    }
 
-const resultado = convertirMoneda(cantidad, monedaOrigen, monedaDestino)
-
-mostrarResultado(cantidad, monedaOrigen, monedaDestino, resultado)
+    if (menu !== '1' && menu !== '2' && menu !== '0') {
+        alert("Opción inválida. Por favor, ingrese una opción válida.")
+    }
+    
+}
